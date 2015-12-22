b6de184dd018e36931907c1b02bbb38f1970e060
Dishali A Damania unknown student <dad29@afsconnect2.njit.edu>
2015-12-21 19:38:57 -0500

    all

--- src/contact-form-proc.php
+++ src/contact-form-proc.php
@@ -1,22 +1,26 @@
 <?php
 
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
 if(empty($_POST['submit']))
 {
 	echo "Form is not submitted!";
 	exit;
 }
 
-if(empty($_POST["fullname"]) || empty($_POST["email"]))
-{
-	echo "please fill the form"
-	exit;
-}
+if(empty($_POST["fullname"]) || 
+	empty($_POST["email"]))
+	{
+		echo "please fill the form";
+		exit;
+	}
 
 $name = $_POST["fullname"];
 $email = $_POST["email"];
 
-mail('dad29@njit.edu', 'New form submission', "New form submission: Name:
-$name, Email: $email");
+mail('dad29@njit.edu', 'New form submission', 'New form submission: Name:
+$name, Email: $email');
 
 header('Location: thank-you.html');
 
